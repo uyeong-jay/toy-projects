8c20f41d1ea7530a45590b4b4eef80a9d7090e33
william-jacob <wjacob2103@gmail.com>
2021-04-26 15:06:22 +0900

    [RENAME]&[UPDATE]

---
+++ calender-clock.js
@@ -0,0 +1,47 @@
+const calender = document.querySelector(".calender"),
+  calenderDate = calender.querySelector(".date");
+
+const clock = document.querySelector(".clock"),
+  clockHour = clock.querySelector(".hour"),
+  clockMinute = clock.querySelector(".minute"),
+  clockSecond = clock.querySelector(".second");
+
+const currentDate = new Date(),
+  year = currentDate.getFullYear(),
+  month = currentDate.getMonth(),
+  date = currentDate.getDate();
+
+function getCurrentDate() {
+  calenderDate.textContent = `${year}-${month < 10 ? `0${month}` : month}-${date < 10 ? `0${date}` : date}`
+}
+
+function getHour() {
+  const currentTime = new Date();
+  const hours = currentTime.getHours();
+  clockHour.textContent = `${hours < 10 ? `0${hours}` : hours}`;
+}
+
+function getMin() {
+  const currentTime = new Date();
+  const minutes = currentTime.getMinutes();
+  clockMinute.textContent = `${minutes < 10 ? `0${minutes}` : minutes}`;
+}
+
+function getSec() {
+  const currentTime = new Date();
+  const seconds = currentTime.getSeconds();
+  clockSecond.textContent = `${seconds < 10 ? `0${seconds}` : seconds}`;
+}
+
+function init1() {
+  getCurrentDate();
+  getHour();
+  getMin();
+  getSec();
+  setInterval(() => {
+    getHour();
+    getMin();
+    getSec();
+  }, 1000);
+}
+init1();
