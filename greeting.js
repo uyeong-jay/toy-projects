7143b893a953679272890baed83c6f038c6f9a4c
william-jacob <wjacob2103@gmail.com>
2021-04-27 11:58:16 +0900

    [ADD}

---
+++ greeting.js
@@ -0,0 +1,9 @@
+const formGreeeting = document.querySelector(".form-greeting"),
+  inputGreeting = formGreeeting.querySelector("input"),
+  greeting = document.querySelector(".greeting");
+
+function initName() {
+  
+}
+
+initName();
