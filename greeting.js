e7773832ae507fc200923d193ae3d81e71a7fa3c
william-jacob <wjacob2103@gmail.com>
2021-04-29 11:22:48 +0900

    [UPDATE] #the function to Save the user name

--- greeting.js
+++ greeting.js
@@ -1,9 +1,43 @@
 const formGreeeting = document.querySelector(".form-greeting"),
-  inputGreeting = formGreeeting.querySelector("input"),
+  inputName = formGreeeting.querySelector("input"),
   greeting = document.querySelector(".greeting");
 
+const USER_LS = "user-name",
+  SHOW_CN = "showing";
+
+function saveName(name) {
+  localStorage.setItem(USER_LS, name);
+}
+
+function handleGreetingSumit(event) {
+  event.preventDefault();
+  const inputNameVal = inputName.value;
+  saveName(inputNameVal);
+  welcome(inputNameVal);
+}
+
+function askForName() {
+  formGreeeting.classList.add(SHOW_CN);
+  formGreeeting.addEventListener("submit", handleGreetingSumit);
+}
+
+function welcome(name) {
+  formGreeeting.classList.remove(SHOW_CN);
+  greeting.classList.add(SHOW_CN);
+  greeting.textContent = `Welcome ${name} 🤗 `;
+}
+
+function loadname() {
+  const getUserName = localStorage.getItem(USER_LS);
+  if (getUserName === null) {
+    askForName();
+  } else {
+    welcome(getUserName);
+  }
+}
+
 function initName() {
-  
+  loadname();
 }
 
 initName();
