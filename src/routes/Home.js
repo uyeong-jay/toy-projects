0b5517773225420057ea18c8ba3d4b6dcaa7056b
william-jacob <wjacob2103@gmail.com>
2021-05-09 15:59:58 +0900

    [UPDATE] added import & class

---
+++ src/routes/Home.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import Movie from '../components/Movie';
+
+class Home extends React.Component {
+  state = {
+    isLoading : true,
+    movies : []
+  };
+
+  
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const {isLoading, movies} = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    )
+  }
+}
+
+export default Home;//
