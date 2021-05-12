559aeb8e5e9eb6cdacef187b8750fc0943b50a5d
william-jacob <wjacob2103@gmail.com>
2021-05-12 15:00:57 +0900

    [UPDATE] added detail page

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -3,13 +3,29 @@ import React from 'react';
 class Detail extends React.Component {
 
   componentDidMount() {
-
+    const {location, history} = this.props;
+    if(location.state === undefined) {
+      history.push("/home");
+    }
   }
 
   render() {
-    return (
-      
-    );
+    const {location : {state}} = this.props;
+    if(state) {
+      return (
+        <div>
+          <div>{state.title}</div>
+          <div>{state.year}</div>
+          <div>{state.genres}</div>
+          <img src={state.poster} title={state.title} alt={state.title} />
+          <p>{state.summary}</p>
+        </div>
+      );
+    } else {
+      return (
+        null
+      );
+    };
   }
 }
 
