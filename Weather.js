135601adebd5db7829839612448670d50aa1e049
william-jacob <wjacob2103@gmail.com>
2021-05-16 22:14:19 +0900

    [UPDATE] 1. added frame

---
+++ Weather.js
@@ -0,0 +1,18 @@
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import PropTyoes from 'prop-types';
+
+export default function Weather({temp, condition}) {
+  return(
+    
+  );
+}
+
+Weather.propTyoes = {
+
+};
+
+const styles = StyleSheet.create({
+  container: {
+
+  }
+});
