aa5718230201e64d16fe450e2cc96a617abed066
william-jacob <wjacob2103@gmail.com>
2021-05-16 22:13:44 +0900

    [UPDATE] 2. added CSS

--- Loading.js
+++ Loading.js
@@ -4,16 +4,22 @@ export default function Loading() {
   return(
     <View style={styles.container}>
       <StatusBar barStyle="dark-content" />
-      <Text style={styles.text}>Loading...</Text>
+      <Text style={styles.text}>Getting Weather...</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "flex-start",
+    paddingHorizontal: 30,
+    paddingVertical: 100,
+    backgroundColor: "skyblue"
   },
   text: {
-
+    color: "#2c2c2c",
+    fontSize: 25
   }
 })
