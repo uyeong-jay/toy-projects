54e298d5c66efcbd3045f3c875025a5bc3bd6f3a
william-jacob <wjacob2103@gmail.com>
2021-05-17 23:00:33 +0900

    [UPDATE] 2. added proptypes and frame of css

--- Weather.js
+++ Weather.js
@@ -8,11 +8,44 @@ export default function Weather({temp, condition}) {
 }
 
 Weather.propTyoes = {
-
+  temp: PropTyoes.number.isRequired,
+  condition: PropTyoes.oneOf([
+    "Thunderstorm",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Atmosphere",
+    "Clear",
+    "Clouds",
+    "Haze",
+    "Mist",
+    "Dust"
+  ]).isRequired
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  halfContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  temp: {
+    
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "flex-start"
+  },
+  title: {
+
+  },
+  subtitle: {
 
   }
 });
