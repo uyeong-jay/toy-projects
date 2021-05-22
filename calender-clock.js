c0823f516a0b4804302fa97e5ad1fd64f1108977
william-jacob <wjacob2103@gmail.com>
2021-05-22 22:48:34 +0900

    [UPDATE] refactoring

--- calender-clock.js
+++ calender-clock.js
@@ -1,10 +1,45 @@
-import {formGreeeting, USER_LS, saveName, handleGreetingSumit, askForName, welcome, loadname, initName} from './greeting.js';
+import {
+  formGreeeting,
+  USER_LS,
+  saveName,
+  handleGreetingSumit,
+  askForName,
+  welcome,
+  loadname,
+  initName
+} from './greeting.js';
 
-import {formToDoList, TODOS_LS, toDos, saveToDos, delToDo, createToDo, handleToDosSubmit, loadToDos, initToDos} from './todo-list.js';
+import {
+  formToDoList,
+  TODOS_LS,
+  toDos,
+  saveToDos,
+  delToDo,
+  createToDo,
+  handleToDosSubmit,
+  loadToDos,
+  initToDos
+} from './todo-list.js';
 
-import {weather, COORDS_LS, getWeather, saveCoords, handleGeoSucces, handleGeoError, askForCoords, loadCoords, initCoords} from './weather.js'
+import {
+  weather,
+  COORDS_LS,
+  getWeather,
+  saveCoords,
+  handleGeoSucces,
+  handleGeoError,
+  askForCoords,
+  loadCoords,
+  initCoords
+} from './weather.js'
 
-import {body, IMG_NUM, images, number, initImg} from './background.js'
+import {
+  container,
+  IMG_NUM,
+  images,
+  number,
+  initImg
+} from './background.js'
 
 const calender = document.querySelector(".calender"),
   calenderDate = calender.querySelector(".date");
@@ -14,30 +49,30 @@ const clock = document.querySelector(".clock"),
   clockMinute = clock.querySelector(".minute"),
   clockSecond = clock.querySelector(".second");
 
-const currentDate = new Date(),
-  year = currentDate.getFullYear(),
-  month = currentDate.getMonth(),
-  date = currentDate.getDate();
 
 function getCurrentDate() {
+  const currentDate = new Date(),
+    year = currentDate.getFullYear(),
+    month = currentDate.getMonth() + 1,
+    date = currentDate.getDate();
   calenderDate.textContent = `${year}-${month < 10 ? `0${month}` : month}-${date < 10 ? `0${date}` : date}`
 }
 
 function getHour() {
-  const currentTime = new Date();
-  const hours = currentTime.getHours();
+  const currentDate = new Date();
+  const hours = currentDate.getHours();
   clockHour.textContent = `${hours < 10 ? `0${hours}` : hours}`;
 }
 
 function getMin() {
-  const currentTime = new Date();
-  const minutes = currentTime.getMinutes();
+  const currentDate = new Date();
+  const minutes = currentDate.getMinutes();
   clockMinute.textContent = `${minutes < 10 ? `0${minutes}` : minutes}`;
 }
 
 function getSec() {
-  const currentTime = new Date();
-  const seconds = currentTime.getSeconds();
+  const currentDate = new Date();
+  const seconds = currentDate.getSeconds();
   clockSecond.textContent = `${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
@@ -47,9 +82,11 @@ function initTime() {
   getMin();
   getSec();
   setInterval(() => {
+    getCurrentDate();
     getHour();
     getMin();
     getSec();
   }, 1000);
 }
+
 initTime();
