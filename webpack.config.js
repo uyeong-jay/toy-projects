b84788159b5521bc32d4a123a7467a2e9a0541b7
william-jacob <wjacob2103@gmail.com>
2021-06-11 22:24:59 +0900

    [UPDATE] I did npm-ren-dev first

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,31 @@
 const path = require('path');
 
 module.exports = {
-  name : "webpack-setting",
-  devtool : "eval",
-  resolve : {},
-  mode : "development",
-  entry : {},
-  module : {},
-  plugins : [],
-  output : {},
+  name: "webpack-setting",
+  devtool: "eval",
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
+  mode: "development",
+  entry: {
+    app: ['./client'],
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      options: {
+        presets: [
+          '@babel/preset-env',
+          '@babel/preset-react',
+        ],
+      },
+    }],
+  },
+  plugins: [],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.js',
+    publicPath: '/dist/'
+  },
 }
