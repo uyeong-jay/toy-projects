5cb4c46c4f45e0c11c69f7dfe5aefa1643b42584
william-jacob <wjacob2103@gmail.com>
2021-06-25 17:26:21 +0900

    [UPDATE] #2 Add click and drawing event

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// 화면 <=> 데이터
 const $table = document.createElement('table');
 const $result = document.createElement('div');
 document.body.append($table);
@@ -16,10 +17,35 @@ for (let i=0; i<3; i++) {
   }
 }
 
-const callback = (event) => {
-  console.log(event.target);
-  // event.target
+let turn = 'O';
+
+const checkWinner = (target) => { // 승자인 경우들
+  const rowIndex = target.parentNode.rowIndex;
+  const cellIndex = target.cellIndex;
+  console.log(rowIndex);
+  console.log(cellIndex);
+  if ()
 }
 
-$table.addEventListener('click', callback); // 이벤트 버블링
+const checkWinnerAndDraw = (target) => { // 승자판단
+  const hasWinner = checkWinner(target);
+  if (hasWinner) {
+    $result.textContent = `${turn}님이 승!`
+  }
+}
+
+const tableCallback = (event) => {
+  // console.log(event.target);2
+  if (!event.target.textContent) {
+    console.log('빔')
+    event.target.textContent = turn;
+    turn = turn === 'O' ? 'X' : 'O';
+  } else {
+    console.log('채워짐')
+    
+  }
+  checkWinnerAndDraw(event.target); // target은 td 
+}
+
+$table.addEventListener('click', tableCallback); // 이벤트 버블링
 
