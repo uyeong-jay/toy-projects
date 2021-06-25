065f7eeb0094cd7f8fd5b96ff7d5f210e3906420
william-jacob <wjacob2103@gmail.com>
2021-06-25 13:36:24 +0900

    [UPDATE] #1 Add 3x3 Table frame

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+const $table = document.createElement('table');
+const $result = document.createElement('div');
+document.body.append($table);
+document.body.append($result);
+const rows = [];
 
+for (let i=0; i<3; i++) {
+  const $tr = document.createElement('tr');
+  $table.append($tr);
+  const cells = [];
+  rows.push(cells);
+  for (let j=0; j<3; j++) {
+    const $td = document.createElement('td');
+    $tr.append($td);
+    cells.push($td);
+  }
+}
 
+const callback = (event) => {
+  console.log(event.target);
+  // event.target
+}
 
-
-
-
-
-
+$table.addEventListener('click', callback); // 이벤트 버블링
 
