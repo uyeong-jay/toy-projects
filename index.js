28cc17cf19694671cdbed41af2a8440dfebd2031
william-jacob <wjacob2103@gmail.com>
2021-06-26 21:33:47 +0900

    [UPDATE]#3 can judge who is the winner

--- index.js
+++ index.js
@@ -17,35 +17,76 @@ for (let i=0; i<3; i++) {
   }
 }
 
+// rows =
+// [ 
+//   [td,td,td], // rowIndex
+//   [td,td,td],
+//   [td,td,td],
+// ]
+
 let turn = 'O';
 
 const checkWinner = (target) => { // 승자인 경우들
   const rowIndex = target.parentNode.rowIndex;
   const cellIndex = target.cellIndex;
+
+  // index로 좌표 표시
   console.log(rowIndex);
   console.log(cellIndex);
-  if ()
-}
+
+  let hasWinner = false; // 승자판단용
+
+  //세칸 모두 찼는지 검사
+  if ( // 가로줄 검사
+    rows[rowIndex][0].textContent === turn &&
+    rows[rowIndex][1].textContent === turn &&
+    rows[rowIndex][2].textContent === turn
+  ) {
+    hasWinner = true;
+  }
+  if ( // 세로줄 검사
+    rows[0][cellIndex].textContent === turn &&
+    rows[1][cellIndex].textContent === turn &&
+    rows[2][cellIndex].textContent === turn 
+  ) {
+    hasWinner = true;
+  }
+  if (
+    rows[0][0].textContent === turn &&
+    rows[1][1].textContent === turn &&
+    rows[2][2].textContent === turn 
+  ) {
+    hasWinner = true;
+  }
+  if (
+    rows[0][2].textContent === turn &&
+    rows[2][0].textContent === turn &&
+    rows[1][1].textContent === turn 
+  ) {
+    hasWinner = true;
+  }
+  return hasWinner;
+};
 
 const checkWinnerAndDraw = (target) => { // 승자판단
   const hasWinner = checkWinner(target);
   if (hasWinner) {
     $result.textContent = `${turn}님이 승!`
+    $table.removeEventListener('click', tableCallback);
   }
-}
+};
 
 const tableCallback = (event) => {
   // console.log(event.target);2
   if (!event.target.textContent) {
-    console.log('빔')
-    event.target.textContent = turn;
-    turn = turn === 'O' ? 'X' : 'O';
+    console.log('넣어짐')
+    event.target.textContent = turn; 
+    checkWinnerAndDraw(event.target); // target은 td 
+    turn = turn === 'O' ? 'X' : 'O'; // 턴 바꿈
   } else {
-    console.log('채워짐')
-    
+    console.log('채워진곳')
   }
-  checkWinnerAndDraw(event.target); // target은 td 
-}
+};
 
 $table.addEventListener('click', tableCallback); // 이벤트 버블링
 
