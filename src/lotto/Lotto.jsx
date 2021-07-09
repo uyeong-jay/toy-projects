1c9a25adbbdbf7b71f04cd7e521c90a311952d18
william-jacob <wjacob2103@gmail.com>
2021-07-09 23:18:28 +0900

    [UPDATE] added getWinNumbers function

--- src/lotto/Lotto.jsx
+++ src/lotto/Lotto.jsx
@@ -4,15 +4,16 @@ import Ball from './Ball';
 
 function getWinNumbers() {
   console.log('getWinNumbers');
-  const candidate = Array(45).fill().map((v,i) => i + 1);// 1 ~ 45 까지 숫자 나열
-  // Array(2)
-  // (2) [empty × 2]
-  // Array(2).fill()
-  // (2) [undefined, undefined]
-  const shuffle = [];
-  while (candidate.length) {
-    
+  const lottoBalls = Array(45).fill().map((v,i) => i + 1);
+  const shuffledBalls = Math.floor(Math.random() * lottoBalls.length)
+  const splicedBalls = lottoBalls.splice(shuffledBalls, 1);
+  const ballsBowl = [];
+  while (lottoBalls.length > 0) {
+    ballsBowl.push(splicedBalls[0]);
   }
+  const winNumbers = ballsBowl.slice(0, 6);
+  const bonusNumber = ballsBowl[ballsBowl.length - 1];
+  return [...winNumbers, bonusNumber]; 
 }
 
 class Lotto extends Component {
