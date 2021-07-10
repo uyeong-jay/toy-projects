2abf4b67c1aff3a90666bda9109af8a34e8ea7cc
william-jacob <wjacob2103@gmail.com>
2021-07-10 23:02:14 +0900

    [FIX] fixed the while part

--- src/lotto/Lotto.jsx
+++ src/lotto/Lotto.jsx
@@ -3,16 +3,20 @@ import Ball from './Ball';
 
 
 function getWinNumbers() {
-  console.log('getWinNumbers');
+
   const lottoBalls = Array(45).fill().map((v,i) => i + 1);
-  const shuffledBalls = Math.floor(Math.random() * lottoBalls.length)
-  const splicedBalls = lottoBalls.splice(shuffledBalls, 1);
+
   const ballsBowl = [];
+  
   while (lottoBalls.length > 0) {
+    const shuffledBalls = Math.floor(Math.random() * lottoBalls.length);
+    const splicedBalls = lottoBalls.splice(shuffledBalls, 1);
     ballsBowl.push(splicedBalls[0]);
   }
+
   const winNumbers = ballsBowl.slice(0, 6);
   const bonusNumber = ballsBowl[ballsBowl.length - 1];
+
   return [...winNumbers, bonusNumber]; 
 }
 
@@ -20,18 +24,28 @@ class Lotto extends Component {
 
   state = {
     WinBalls: [],
-    
+    BonusBall: null, 
   };
 
+  const onClickRedo = () => {
+
+  }
+
   render() {
+    const { WinBalls, BonusBall } = this.state;
     return(
       <>
-        <div>로또 번호 :</div>
+        <h4>로또 번호 :</h4>
         <div>{WinBalls}</div>
-        <div>보너스 번호 : </div>
+        <h6>보너스 번호 : </h6>
+        <div>{BonusBall}</div>
+        <button onclick={onClickRedo}></button>
+
         <Ball />
       </>
     )
   }
 }
+
+
 export default Lotto;
