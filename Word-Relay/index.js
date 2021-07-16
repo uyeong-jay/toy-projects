3d55cf76d41bb546387f58533d0a719c58d7f967
william-jacob <wjacob2103@gmail.com>
2021-07-16 22:54:59 +0900

    [UPDATE] added basic frame

---
+++ Word-Relay/index.js
@@ -0,0 +1,5 @@
+const numberOfParticipants = parseInt(prompt('몇명이 참가하실건가요?'),10);
+console.log('numberOfParticipants', numberOfParticipants);
+const alertNumberOfParticipants = alert(numberOfParticipants);
+const confirmNumberOfParticipants = confirm(`${numberOfParticipants}명이 맞으신가요?`);
+
