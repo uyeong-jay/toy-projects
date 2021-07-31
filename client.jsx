98335849efd8f34757f5947981a0be92ee6fdc66
william-jacob <wjacob2103@gmail.com>
2021-07-31 18:35:08 +0900

    [FIX] fixed the type error

--- client.jsx
+++ client.jsx
@@ -3,4 +3,4 @@ const ReactDom = rquire('react-dom');
 
 const TestWebpack = require('./TestWebpack'); 
 
-ReactDom.render(<TestWebpack />, documnet.querySelector('root')); //화면에 그려줌
+ReactDom.render(<TestWebpack />, documnet.querySelector('#root')); //화면에 그려줌
