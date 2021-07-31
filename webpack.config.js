5c19471f35e4e0869dff26579015fc41dea1b9d2
william-jacob <wjacob2103@gmail.com>
2021-07-31 18:12:35 +0900

    [UPDATE] added module to work babel

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,17 @@ module.exports = {
     app: ['./client'],
   },
 
+  module: {
+    rules: [{
+      test: /\.jsx?/,
+      loader: 'babal-loader',
+      option: {
+        presets: [ '@babel/preset-env', '@babel/preset-react' ],
+        plugins: [],
+      },
+    }],
+  },
+
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'app.js',
