d61f1af632094e12269db62ffcfd526ba717a83f
william-jacob <wjacob2103@gmail.com>
2021-07-31 21:53:59 +0900

    [UPDATE] set browser lists in babel/preset-env

--- webpack.config.js
+++ webpack.config.js
@@ -17,16 +17,25 @@ module.exports = {
       test: /\.jsx?$/,
       loader: 'babel-loader',
       options: {
-        presets: [ '@babel/preset-env', '@babel/preset-react' ],
+        presets: 
+        [ 
+          [
+            '@babel/preset-env', 
+            { 
+              targets: { browsers: [ "> 5% in KR" ] },
+              debug: true,
+            },
+          ], 
+          '@babel/preset-react' 
+        ],
         // plugins: [''],
       },
     }],
   },
-
+  
   output: {
     path: path.join(__dirname, './dist'),
     filename: 'app.js',
   },
 
-  
 }
