18e06c15862f20b09e5fb9f82877f41aa6227427
william-jacob <wjacob2103@gmail.com>
2021-07-31 18:07:13 +0900

    [UPDATE] added state

--- TestWebpack.jsx
+++ TestWebpack.jsx
@@ -2,7 +2,15 @@ const React = require('react');
 const { Component } = React;
 
 class TestWebpack extends Component {
+  state = {
+    text: 'Hello webpack'
+  }
 
+  render() {
+    return(
+      <div>{text}</div>
+    );
+  }
 }
 
 module.exports = TestWebpack;
