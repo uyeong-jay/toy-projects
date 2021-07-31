83090abdbaab769356793ac8ba3fbb6805b92af8
william-jacob <wjacob2103@gmail.com>
2021-07-31 18:09:02 +0900

    [UPDATE] added basic setting about webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,21 @@
+const path = require('path');
+
 module.exports = {
+  name: 'test-webpack',
+  mode: 'development', // 배포용: 'production' 
+  devtool: 'evel',
+  resolve: {
+    extensions: [ '.jsx', '.js' ],
+  },
+
+  entry: {
+    app: ['./client'],
+  },
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.js',
+  },
+
   
 }
