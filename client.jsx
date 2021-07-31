00fbc33eaa13fe079fa2f0aec8fd6685263fcb23
william-jacob <wjacob2103@gmail.com>
2021-07-31 17:03:28 +0900

    [UPDATE] connected TestWebpack file

--- client.jsx
+++ client.jsx
@@ -1,2 +1,6 @@
 const React = rquire('react');
 const ReactDom = rquire('react-dom');
+
+const TestWebpack = require('./TestWebpack'); 
+
+ReactDom.render(<TestWebpack />, documnet.querySelector('root')); //화면에 그려줌
