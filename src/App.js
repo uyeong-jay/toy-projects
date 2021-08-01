1b769e59b47332144255fd7a719f4ef391b74f0d
william-jacob <wjacob2103@gmail.com>
2021-08-01 21:34:36 +0900

    [UPDATE] added GuGudan class-ver and hooks-ver

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import Lotto from './Lotto';
+// import Lotto from './lotto/Lotto';
+import GuGuDanClass from './gugudan/GuGuDanClass';
+import GuGuDanHooks from './gugudan/GuGuDanHooks';
+import WordRelayClass from './wordRelay/WordRelayClass';
+
 
 function App() {
   return (
     <>
-      <Lotto />
+      <h2>1.GuGuDan</h2>
+      <span>Class Version</span>
+      <GuGuDanClass />
+      <br />
+      <span>Hooks Version</span>
+      <GuGuDanHooks />
+      <br />
+      <br />
+      <h2>2.Word-Relay</h2>
+      <span>Class Version</span>
+      <WordRelayClass />
+      <br />
+      <span>Hooks Version</span>
+
+
+      {/* <Lotto /> */}
     </>
   );
 }
