c2c44dbe701dfcd0ed91ea240852c868d781133e
william-jacob <wjacob2103@gmail.com>
2021-08-10 23:04:31 +0900

    [UPDATE] import createStore and added types

---
+++ todo.js
@@ -0,0 +1,28 @@
+//createStore
+import { createStore } from 'redux';
+
+const formToDo = document.querySelector('#formToDo');
+const inputToDo = document.querySelector('input');
+const ulToDo = document.querySelector('#ulToDo');
+
+
+//type
+const ADD_TODO = 'ADD_TODO';
+const DELETE_TODO = 'DELETE_TODO';
+
+//action
+
+
+
+//reducer
+
+
+
+//store
+
+
+
+//dispatch
+
+
+//subscribe
