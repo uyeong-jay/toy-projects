498f2a3158af20f5cf27a8e09225687e7e4a148b
william-jacob <wjacob2103@gmail.com>
2021-08-22 21:29:04 +0900

    [UPDATE] added BrowserRouter, Route, Link and Query string

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import ImageUpload1 from './imageUpload/ImageUpload';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import ImageUpload from './imageUpload/ImageUpload';
 
 const App = () => {
   return (
-    <>
-      <ImageUpload1 />
-    </>
+    <BrowserRouter>
+      <div>
+        {/* 공통 layout */}
+        <Link to='/image/upload1/?query=10&hello=woo&bye=yeong'>Image Upload1</Link>
+        <br />
+        <Link to='/image/upload2'>Image Upload2</Link>
+      </div>
+      <div>
+        {/* 바뀌는 layout */}
+        <Route path='/image/:upload' component={ImageUpload} />
+      </div>
+    </BrowserRouter>
   );
 }
 
