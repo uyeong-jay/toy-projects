21ebaa287e0d0a9bf66770bc45dae177ac2ef030
william-jacob <wjacob2103@gmail.com>
2021-08-28 23:15:09 +0900

    [ADD] added Number-Baseball folder

---
+++ Number-Baseball/index.js
@@ -0,0 +1,7 @@
+const $form = document.querySelector('#form');
+const $input = document.querySelector('#input');
+const $button = document.querySelector('#button');
+const $logs = document.querySelector('#logs');
+
+
+$form.addEventListener('')
