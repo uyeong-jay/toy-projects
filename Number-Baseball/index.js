11d8bf745c7394147d4a932ef7e7ade7ca713311
william-jacob <wjacob2103@gmail.com>
2021-08-29 21:17:47 +0900

    [UPDATE] picked numbers randomly and added submit handler func

--- Number-Baseball/index.js
+++ Number-Baseball/index.js
@@ -3,5 +3,25 @@ const $input = document.querySelector('#input');
 const $button = document.querySelector('#button');
 const $logs = document.querySelector('#logs');
 
+const numbers = []; // 1~9
+for (let n = 0; n < 9; n++) {
+  numbers.push(n+1);
+}
 
-$form.addEventListener('')
+const answer = []; // numbers의 1~9중 랜덤하게 4개만 answer에 넣기
+for (let i = 0; i < 4; i++) {
+  const index = Math.floor(Math.random() * numbers.length);
+  answer.push(numbers[index]);
+  numbers.splice(index, 1);
+}
+
+
+
+const submitHandler = (e) => {
+  e.preventDefault();
+  const value = $input.value
+  input.value = '';
+  
+}
+
+$form.addEventListener('submit', submitHandler);
