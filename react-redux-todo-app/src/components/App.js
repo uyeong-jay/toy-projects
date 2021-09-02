1207a5e06b33546e0ad6465441d338e139444fa2
william-jacob <wjacob2103@gmail.com>
2021-09-02 23:18:16 +0900

    [UPDATE] applied Route

--- react-redux-todo-app/src/components/App.js
+++ react-redux-todo-app/src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from '../routes/Home';
+import Detail from '../routes/Detail';
 
 const App = () => {
   return(
-    <>
-      App
-    </>
+    <Router>
+      <Route path='/' exact component={Home} />
+      <Route path='/:id' exact component={Detail} />
+    </Router>
   )
 }
 
