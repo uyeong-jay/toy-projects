dbfb8e456f7d513d957b61a88dce6f8741210209
william-jacob <wjacob2103@gmail.com>
2021-09-04 23:37:46 +0900

    [UPDTAE] added functions to manage logs and compare input value

--- Number-Baseball/index.js
+++ Number-Baseball/index.js
@@ -14,43 +14,68 @@ for (let i = 0; i < 4; i++) {
   answer.push(numbers[index]);
   numbers.splice(index, 1);
 }
-if (answer) {
-  confirm(`숫자야구의 답이 준비되었습니다. 시작하시겠습니까?`);
-
-  const tries = [];
 
 
-  const checkInput = (inputVal) => {
-    if (inputVal.length !== 4) {
-      return alert('4자리 숫자를 입력해 주세요');
-    }
-    if (new Set(inputVal).size !== 4) {
-      return alert('중복되지 않게 입력해 주세요.');
-    }
-  if (tries.includes(input)) {
-      return alert('이미 시도한 값입니다.');
-    }
-    return true;
+const tries = [];
+let strike = 0;
+let ball = 0;
+let out = 0;
+
+const compareInput = (inputVal) => {
+  if (answer.join('') === inputVal) {
+    $logs.textContent = "홈런! ";
+    return;
   }
-
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-    const inputVal = $input.value
-    input.value = '';
-    if (checkInput(inputVal)) {
-      if (answer.join('') === value) {
-        $logs.textContent = "홈런!";
-        return;
+  for (let i = 0; i < answer.length; i++) {
+    const index = inputVal.indexOf(answer[i]);
+    if (index > -1) {
+      if (inputVal[index] === answer.join('')[index]) {
+        strike++;
+      } else {
+        ball++;
       }
-      if (tries.length >= 9) {
-        
-      }
-      tries.push(inputVal);
     } else {
-      alert('땡!');
+      out++;
     }
   }
-
-  $form.addEventListener('submit', submitHandler);
 }
+
+
+const manageLogs = (inputVal) => {
+  tries.push(inputVal);
+  compareInput(inputVal);
+  $logs.setAttribute('style', 'white-space: pre;');
+  $logs.textContent += `${inputVal},${answer.join('')} => strike :${strike}, ball :${ball}, out :${out}\r\n`;
+  strike = 0;
+  ball = 0;
+  out = 0;
+  if (tries.length >= 10) {
+    $logs.textContent = `기회 초과! 실패입니다! \n 답은 : ${answer.join('')} 였습니다.`;
+    return;
+  }
+}
+
+
+const checkInput = (inputVal) => {
+  if (inputVal.length !== 4) {
+    return alert('4자리 숫자를 입력해 주세요');
+  }
+  if (new Set(inputVal).size !== 4) {
+    return alert('중복되지 않게 입력해 주세요.');
+  }
+  if (tries.includes(input)) {
+    return alert('이미 시도한 값입니다.');
+  }
+  manageLogs(inputVal);
+}
+
+
+const submitHandler = (e) => {
+  e.preventDefault();
+  const inputValue = $input.value; //데이터
+  console.log(inputValue);
+  input.value = ''; //화면
+  checkInput(inputValue);
+}
+
+$form.addEventListener('submit', submitHandler);
