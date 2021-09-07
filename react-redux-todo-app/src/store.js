fd0e0f6e846ffb0295de0e454fb5346a47f89c47
william-jacob <wjacob2103@gmail.com>
2021-09-07 23:25:10 +0900

    [ADD && UPDATE] added store.js and redux basic frame

---
+++ react-redux-todo-app/src/store.js
@@ -0,0 +1,34 @@
+//createStore
+import { createStore } from 'redux';
+
+//type
+const ADD = 'ADD';
+const DELETE = 'DELETE';
+
+//action-creator
+const addToDo = () => {
+  return {
+
+  }
+};
+const deleteToDo = () => {
+  return {
+
+  }
+};
+
+//reducer
+const reducer = (state=[], action) = {
+  switch() {
+    case:
+  }
+}
+
+//store
+const store = createStore(reducer);
+
+//action-creators
+const actionCreators = {
+  addToDo,
+  deleteToDo,
+}
