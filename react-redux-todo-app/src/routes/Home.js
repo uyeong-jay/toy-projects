87882f01d2956a035f1bc3e7a29e8c281979665d
william-jacob <wjacob2103@gmail.com>
2021-09-16 22:58:28 +0900

    [UPDATE] added todolist form

--- react-redux-todo-app/src/routes/Home.js
+++ react-redux-todo-app/src/routes/Home.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { connect } from 'react-redux';
 import actionCreators from '../store';
 
-const Home = ({ dispatchAddToDo }) => {
+const Home = ({ toDos, dispatchAddToDo }) => {
+
+  const [text, setText] = useState('');
+  const onInputRef = useRef(null);
+
+  const onSubmitText = (e) => {
+    e.preventDefault();
+    dispatchAddToDo(text);
+    onInputRef.current.focus();
+  }
+
+  const onChangeText = (e) => {
+    setText(e.currentTarget.value);
+  }
+
   return(
     <>
-      Home
+      <h3>To Do</h3>
+      <form onSubmit={onSubmitText}>
+        <input type="text" name="todo" value={text} onChange={onChangeText} ref={onInputRef} />
+        <button type="submit" >입력</button>
+      </form>
     </>
   )
 }
