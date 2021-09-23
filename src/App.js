45245a33b28eb4ba6329c24d83dbe2cba6530c6b
william-jacob <wjacob2103@gmail.com>
2021-09-23 22:46:35 +0900

    [UPDATE] rendered bunch of components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import ToDoTemplete from './components/ToDoTemplete';
+import ToDoHead from './components/ToDoHead';
+import ToDoList from './components/ToDoList';
+import ToDoCreate from './components/ToDoCreate';
+
+import { createGlobalStyle } from 'styled-components';
+
+
+//직접 Dom에 접근가능
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <ToDoTemplete>
+      <GlobalStyle />
+      <ToDoHead />
+      <ToDoList />
+      <ToDoCreate />
+    </ToDoTemplete>
   );
 }
 
