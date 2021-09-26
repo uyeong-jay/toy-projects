8f2c40136630b0fd88c88f05fefcb477606e4f23
william-jacob <wjacob2103@gmail.com>
2021-09-26 23:19:04 +0900

    [UPDATE] added today's date

--- src/components/ToDoHead.js
+++ src/components/ToDoHead.js
@@ -1,38 +1,46 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import { useToDoState } from "../hooks/useToDoContext";
 
 const ToDoHeadBlock = styled.div`
-  border: 1px solid black;
   flex: 0.25;
   padding: 50px 0px 10px 25px;
   border-bottom: 1px solid #e9ecef;
   h1 {
-    border: 1px solid black;
     margin: 0;
     font-size: 36px;
     color: #343a40;
   }
   .day {
-    border: 1px solid black;
     font-size: 21px;
     color: #868e96;
   }
   .tasks-left {
-    border: 1px solid black;
-    padding-top: 40px; 
+    padding-top: 40px;
     font-size: 18px;
     color: #20c997;
   }
 `;
 
 const ToDoHead = () => {
-  return(
+  const toDoState = useToDoState();
+  const todayToDos = toDoState.filter((v, i) => v.done === false).length;
+
+  const date = new Date();
+  const todayDateString = date.toLocaleString("ko-KR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  const todayWeekdayString = date.toLocaleString("ko-KR", { weekday: "long" });
+
+  return (
     <ToDoHeadBlock>
-      <h1>2021년 9월 22일</h1>
-      <div className="day">수요일</div>
-      <div className="tasks-left">오늘 할일: 2</div>
+      <h1>{todayDateString}</h1>
+      <div className="day">{todayWeekdayString}</div>
+      <div className="tasks-left">오늘 할일: {todayToDos}</div>
     </ToDoHeadBlock>
   );
-}
+};
 
 export default ToDoHead;
