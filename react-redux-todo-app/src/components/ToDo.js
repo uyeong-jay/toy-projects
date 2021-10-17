a5962d206b2f4aacb9b08c6c0a471ebd9d35fb5f
william-jacob <wjacob2103@gmail.com>
2021-10-17 22:56:27 +0900

    [UPDATE] imported Link and  connect

--- react-redux-todo-app/src/components/ToDo.js
+++ react-redux-todo-app/src/components/ToDo.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { actionCreators } from '../store';
+import React from "react";
+import { connect } from "react-redux";
+import { actionCreators } from "../store";
+import { Link } from "react-router-dom";
 
-const ToDo = ({ dispatchDeleteToDo }) => {
+const ToDo = ({ dispatchDeleteToDo, toDoText, toDoId }) => {
 
-
-  return(
-    <>
-      ToDo
-    </>
+  return (
+    <li>
+      <Link to={`/${toDoId}`}>
+        {toDoText}
+      </Link>
+      <button onClick={dispatchDeleteToDo}>DEL</button>
+    </li>
   );
-}
+};
 
 //disptach 기능 추가
+//ownProps: 자신의 컴포넌트 props 정보를 전달해줌
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
     dispatchDeleteToDo: () => {
-      dispatch(actionCreators.deleteToDo(ownProps.id))
-    }
-  }
-}
+      dispatch(actionCreators.deleteToDo(ownProps.toDoId));
+    },
+  };
+};
 
 //connect로 store.js에 접근
-export default connect(null, mapDispatchToProps) (ToDo);
+export default connect(null, mapDispatchToProps)(ToDo);
