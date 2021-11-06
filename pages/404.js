ec3afdd2e47fea190677dc9f44f131e52449e862
william-jacob <wjacob2103@gmail.com>
2021-11-06 16:02:39 +0900

    [ADD && UPDATE] custom 404 error page

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import utilStyles from '../styles/utils.module.css'
+
+
+export default function Custom404() {
+  return (
+    <>
+      <h1 className={utilStyles.headingXl}>404 - Page Not Found</h1>
+      <p>죄송합니당. 이 페이지는 찾을 수 없습니당.</p>
+    </>
+  );
+}
