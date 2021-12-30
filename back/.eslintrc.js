c521201d51535211faa9f2ce8cf1a234b3284b04
william-jacob <wjacob2103@gmail.com>
2021-12-30 17:58:26 +0900

    [UPDATE] added more import options

--- back/.eslintrc.js
+++ back/.eslintrc.js
@@ -18,8 +18,39 @@ module.exports = {
   },
   plugins: ['@typescript-eslint', 'prettier', 'import'],
   rules: {
-    'prettier/prettier': 'error',
-    'import/extensions': 'off',
     'no-console': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'import/extensions': 'off',
+    'import/no-unresolved': 'error',
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'never',
+        groups: [
+          ['builtin', 'external'],
+          ['internal', 'parent', 'sibling', 'index'],
+        ],
+      },
+    ],
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+
+        // Choose from one of the 'project' configs below or omit to use <root>/tsconfig.json by default
+
+        // use <root>/path/to/folder/tsconfig.json
+        project: './tsconfig.json',
+      },
+    },
   },
 };
