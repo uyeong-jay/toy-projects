128045a9cc0fc5b890224f4bbdd8001b252b5af2
william-jacob <wjacob2103@gmail.com>
2022-04-20 20:15:17 +0900

    feat: added user model

---
+++ react-blog-app/server/src/models/userModel.ts
@@ -0,0 +1,46 @@
+import { Schema, model } from "mongoose";
+
+// 1. Create an interface representing a document in MongoDB.
+interface User {
+  name: object;
+  account: object;
+  password: object;
+  avatar: object;
+  type: object;
+}
+
+// 2. Create a Schema corresponding to the document interface.
+const userSchema = new Schema<User>(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add your name"],
+      trim: true,
+      maxLength: [20, "Your name can be up to 20 chars long"],
+    },
+    account: {
+      type: String,
+      required: [true, "Please add your email or phone number"],
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please add your password"],
+      trim: true,
+    },
+    avatar: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/uyeong/image/upload/v1637676343/nextjs_media/igin1evr3clomdfy2ikm.png",
+    },
+    type: {
+      type: String,
+      default: "normal",
+    },
+  },
+  { timestamps: true }
+);
+
+// 3. Create a Model.
+export default model("User", userSchema);
