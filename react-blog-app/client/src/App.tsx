cf42309f5a9a72ff01e5f6cd140a3db24f42ed27
william-jacob <wjacob2103@gmail.com>
2022-06-06 22:22:30 +0900

    feat: added page renderer

--- react-blog-app/client/src/App.tsx
+++ react-blog-app/client/src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Page from "./PageRenderer";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Page />} />
+        <Route path="/:page" element={<Page />} />
+        <Route path="/:page/:slug" element={<Page />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ react-blog-app/client/src/PageRenderer.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import NotFound from "./components/global/NotFound";
+
+// https://lakelouise.tistory.com/84 - React.creatElement
+// https://gomgomkim.tistory.com/11 - require(`./pages/${name}`).default
+const generatePage = (name: string) => {
+  const component = () => require(`./pages/${name}`).default;
+
+  try {
+    return React.createElement(component());
+  } catch (err) {
+    return <NotFound />;
+  }
+};
+
+const PageRenderer = () => {
+  const { page, slug } = useParams();
+  let name = "";
+  if (page) name = slug ? `${page}/${slug}` : `${page}`;
+
+  return generatePage(name);
+};
+
+export default PageRenderer;
