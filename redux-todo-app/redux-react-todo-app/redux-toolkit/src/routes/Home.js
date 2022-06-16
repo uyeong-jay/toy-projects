22fc49a9cef6c2f1d0d61ab0526a49441b61cb5e
william-jacob <wjacob2103@gmail.com>
2022-06-17 00:49:32 +0900

    rename: added react-redux, redux-toolkit foleders
    the src folder is redux-toolkit folder

---
+++ redux-todo-app/redux-react-todo-app/redux-toolkit/src/routes/Home.js
@@ -0,0 +1,58 @@
+import React, { useRef, useState } from "react";
+import { connect } from "react-redux";
+import { add } from "../store";
+import ToDo from "../components/ToDo";
+
+const Home = ({ toDos, dispatchAddToDo }) => {
+  const [text, setText] = useState("");
+  const onRefInput = useRef(null);
+
+  const onSubmitText = (e) => {
+    e.preventDefault();
+    dispatchAddToDo(text);
+    onRefInput.current.focus();
+    setText("");
+  };
+
+  const onChangeText = (e) => {
+    setText(e.currentTarget.value);
+  };
+
+  return (
+    <>
+      <h3>To Do</h3>
+
+      <form onSubmit={onSubmitText}>
+        <input
+          type="text"
+          name="todo"
+          value={text}
+          onChange={onChangeText}
+          ref={onRefInput}
+        />
+        <button type="submit">입력</button>
+      </form>
+
+      <ul>
+        {toDos.map((v, i) => (
+          <ToDo key={v.id} toDoText={v.text} toDoId={v.id} />
+        ))}
+      </ul>
+    </>
+  );
+};
+
+//state 받아오기
+const mapStateToProps = (state) => {
+  return { toDos: state };
+};
+
+//dispatch 기능 추가
+//첫 사용자의 text를 보낸땐 ownProps를 쓰지 않음
+const mapDispatchToProps = (dispatch) => {
+  return {
+    dispatchAddToDo: (text) => dispatch(add(text)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
---
+++ redux-todo-app/redux-react-todo-app/redux-toolkit/src/store.js
@@ -0,0 +1,36 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const toDos = createSlice({
+  name: "toDosReducer",
+  initialState: [],
+  //mutate, immutate 둘다 가능
+  reducers: {
+    add: {
+      reducer: (state, action) => {
+        state.push({
+          text: action.payload.text,
+          id: action.payload.id,
+        });
+      },
+
+      //customizing action
+      prepare: (text) => {
+        const id = Date.now();
+        return { payload: { id, text } };
+        //text말고도 id까지 action에서 가져올수 있도록 만듬
+      },
+    },
+
+    remove: (state, action) => {
+      return state.filter((toDo) => toDo.id !== action.payload);
+    },
+  },
+});
+
+//store생성(toDos.reducer)
+export const store = configureStore({
+  reducer: toDos.reducer,
+});
+
+//action들 모음(toDos.actions)
+export const { add, remove } = toDos.actions;
