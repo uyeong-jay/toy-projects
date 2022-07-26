218ad42d25e7febb30ee7d03ccd90b0f07e4d955
william-jacob <wjacob2103@gmail.com>
2022-07-26 22:10:03 +0900

    feat: added logger middleware

--- redux-toolkit-tutorial/react-redux-toolkit-ts-with-vite/src/app/store.ts
+++ redux-toolkit-tutorial/react-redux-toolkit-ts-with-vite/src/app/store.ts
@@ -4,15 +4,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 //미들웨어: logger 불러오기
-// import reduxLogger from "redux-logger";
+import reduxLogger from "redux-logger";
 
 //2. 자신의 Slice에서 reducer 불러오기
 import cakeReducer from "../features/cake/cakeSlice";
 import coffeeReducer from "../features/coffee/coffeeSlice";
 import userReducer from "../features/user/userSlice";
-
-//미들웨어: logger 생성하기
-// const logger = reduxLogger.createLogger();
 
 //3. configureStore에 불러온 reducer 저장하기
 //  - redux의 combine reducer 와 비슷한 역할
@@ -23,7 +20,8 @@ const store = configureStore({
     user: userReducer,
   },
   //미들웨어: logger 추가하기
-  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(reduxLogger),
 });
 
 //+ Add types
