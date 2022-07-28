3863616e281a5d4ceb37be1ec76c174811f6082a
william-jacob <wjacob2103@gmail.com>
2022-07-28 16:05:41 +0900

    delete: deleted all

--- react-storybook-tutorial/src/components/Decorators/Button/ButtonLayout.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./ButtonLayout.css";
-
-const ButtonLayout = ({ children }) => {
-  return <div className="button-layout">{children}</div>;
-};
-
-export default ButtonLayout;
